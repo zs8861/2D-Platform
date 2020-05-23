c209af63e0e090a6575a1a4aa3d5a267774ceccc
zheng shuo <117711668.com>
2020-05-23 19:11:20 +0800

    Unity实现背景图片平滑过渡

---
+++ Assets/Script/ScreenChange.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenChange : MonoBehaviour
+{
+    public GameObject img1;
+    public GameObject img2;
+    public float time;
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            anim.SetBool("ChangeToWhite", true);
+            Invoke("ChangeImage", time);
+        }
+    }
+
+    void ChangeImage()
+    {
+        img1.SetActive(false);
+        img2.SetActive(true);
+    }
+}
