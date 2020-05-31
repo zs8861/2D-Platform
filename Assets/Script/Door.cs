f975f945c512c9b29006e087ceb38f6567bd21cd
zheng shuo <117711668.com>
2020-05-31 17:25:52 +0800

    实现Scene场景切换

---
+++ Assets/Script/Door.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player")
+            && other.GetType().ToString() == "UnityEngine.CapsuleCollider2D")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
