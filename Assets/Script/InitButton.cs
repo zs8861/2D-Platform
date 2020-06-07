c33b255f58a8d1ccd2117e25842159da38fdca38
zheng shuo <117711668.com>
2020-06-07 17:48:41 +0800

    实现开始菜单功能-完整版

---
+++ Assets/Script/InitButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InitButton : MonoBehaviour
+{
+    private GameObject lastSelect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastSelect = new GameObject();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(EventSystem.current.currentSelectedGameObject == null)
+        {
+            EventSystem.current.SetSelectedGameObject(lastSelect);
+        }
+        else
+        {
+            lastSelect = EventSystem.current.currentSelectedGameObject;
+        }
+    }
+}
---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
