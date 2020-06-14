ebd5efb3f34e948a6e1c6097f498d710f8c7fa81
zheng shuo <117711668.com>
2020-06-14 13:48:41 +0800

    完成UI进度条功能

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -2,12 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            Debug.Log(operation.progress);
+            float progress = operation.progress / 0.9f;
+            slider.value = progress;
+            progressText.text = Mathf.FloorToInt(progress * 100f) + "%";
+            //Debug.Log(progressText.text);
+            yield return null;
+        }
     }
 
     public void QuitGame()
