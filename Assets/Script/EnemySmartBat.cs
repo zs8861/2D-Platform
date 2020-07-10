32518eb24ebdbf4059f45b9a8958c203679b93c7
zheng shuo <117711668.com>
2020-07-10 08:42:31 +0800

    实现靠近敌人时，敌人追击Player的功能

---
+++ Assets/Script/EnemySmartBat.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySmartBat : Enemy
+{
+    public float speed;
+    public float radius;
+
+    private Transform playerTransform;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        base.Start();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        //调用父类的Update()方法
+        base.Update();
+        if (playerTransform != null)
+        {
+            float distance = (transform.position - playerTransform.position).sqrMagnitude;
+
+            if(distance < radius)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, 
+                                                        playerTransform.position,
+                                                        speed * Time.deltaTime);
+            }
+        }
+    }
+}
+
