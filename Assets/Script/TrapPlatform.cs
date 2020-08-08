5212f3acc9b2f051cd443a214ab3d3958c77a2c1
zheng shuo <117711668.com>
2020-08-08 14:58:37 +0800

    实现破碎陷阱平台功能

---
+++ Assets/Script/TrapPlatform.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapPlatform : MonoBehaviour
+{
+    private BoxCollider2D bx2D;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bx2D = GetComponent<BoxCollider2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") 
+            && other.GetType().ToString() == "UnityEngine.BoxCollider2D")
+        {
+            anim.SetTrigger("Collapse");
+        }
+    }
+
+    void DisableBoxCollider()
+    {
+        bx2D.enabled = false;
+    }
+
+    void DestroyTrapPlatform()
+    {
+        Destroy(gameObject);
+    }
+}
