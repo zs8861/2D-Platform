d71772945a856bed661783f82c13254b4aa3b1d8
zheng shuo <117711668.com>
2020-08-20 09:16:44 +0800

    完成鼠标点击射击子弹功能

---
+++ Assets/Script/Gun.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform muzzleTransform;
+    public Camera cam;
+
+    private Vector3 mousePos;
+    private Vector2 gunDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = cam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        gunDirection = (mousePos - transform.position).normalized;
+        float angle = Mathf.Atan2(gunDirection.y, gunDirection.x) * Mathf.Rad2Deg;
+        transform.eulerAngles = new Vector3(0, 0, angle);
+
+        if(Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Debug.Log("鼠标左键已经按下");
+            //生成一颗子弹
+            Instantiate(bullet, muzzleTransform.position, Quaternion.Euler(transform.eulerAngles));
+        }
+    }
+}
